855fbcf0f57856e1b5066382dee72f2334cd9d09
vitormvrdev <vitormvr2001@gmail.com>
2025-11-30 10:27:27 +0000

    feat: add initial project structure with controllers, models, routes, and views

---
+++ src/controllers/auth/authController.js
@@ -0,0 +1,51 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+exports.register = async (req, res) => {
+    // Receber os dados do formulário (ou do Postman)
+    const { firstName, lastName, username, email, password } = req.body;
+
+    // Validação básica
+    if (!firstName || !lastName || !username || !email || !password) {
+        return res.status(400).json({ message: "Por favor preencha todos os campos." });
+    }
+
+    try {
+        // Verificar se o user já existe
+        const userExists = await User.findOne({ 
+            $or: [{ email: email }, { username: username }] 
+        });
+
+        if (userExists) {
+            return res.status(400).json({ message: "Username ou Email já registado." });
+        }
+
+        // Encriptar a password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Criar o Utilizador
+        const user = await User.create({
+            firstName,
+            lastName,
+            username,
+            email,
+            password: hashedPassword // Guardamos a versão encriptada!
+        });
+
+        // Sucesso!
+        // Mais tarde aqui vamos gerar o token JWT. Para já, devolvemos o user.
+        res.status(201).json({
+            success: true,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Erro ao registar utilizador." });
+    }
+};
